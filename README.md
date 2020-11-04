67930fe489fa67eb9c184708d59658ee0ff95a20
JL <jennalbouche@gmail.com>
2020-11-04 12:51:29 -0800

    Update REadme, update html, update css, update js to add functions and lists

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-#Mario Matching
+# Let's Play Mario Memory!
 
-##What is Mario Matching?
+## What is Mario Memory?
 
-###Mario matching is a take on the classic Memory game, designed to test your memory with a deck of cards to see if you can find each card's match.
+![Confused Mario](https://www.nicepng.com/png/detail/150-1509652_vr-retro-games-confused-mario.png)
 
-##Instructions:
+### Mario memory is a take on the classic Memory game, designed to test your memory with a deck of cards to see if you can find each card's match.
 
-###Click on two face-down cards at a time to turn them face-up. If it's a match, they will stay face-up! Otherwise, they will return to face-down and you will have to guess again to find the correct match. Try to remember what card is where! You have 3 'lives' to make an incorrect guess. Good luck!
+## Instructions:
 
+![Mario reading](https://i0.wp.com/www.infendo.com/wp-content/uploads/2012/01/paper_mario_reading_map_preview.png?resize=256%2C256)
+
+### Click on two face-down cards at a time to turn them face-up. If it's a match, they will stay face-up! Otherwise, they will return to face-down and you will have to guess again to find the correct match. Try to remember what card is where! You have 3 'lives' to make an incorrect guess. Good luck!
--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 /*----- constants -----*/
 const maxLives = 3;
-
+const card = document.getElementsByClassName('card');
+let cards = [...card];
+const deck = document.getElementById('card-deck');
 
 /*----- app's state (variables) -----*/
+let wrongMoves = 0;
+let openedCards = [];
+let countWrongMoves = document.querySelector('.livesLeft')
 
 
 /*----- cached element references -----*/
-const cardDeck = document.getElementsByClassName('deck');
 const playGame = document.getElementById('start-game');
 
 /*----- event listeners -----*/
 playGame.addEventListener('click', startGame);
+for (let i = 0; i < cards.length; i++){
+    cards[i].addEventListener('click', displayCard);
+}
+
 
 /*----- functions -----*/
-init();
+/*init();
 
 function init(e) {
     render();
@@ -22,21 +30,54 @@ function init(e) {
 
 function render() {
     console.log('render is firing');
-}
+}*/
 
 function startGame() {
-    shuffleDeck(cardDeck);
-    marioAudio();
+    startingAudio();
+    cards = shuffleDeck(cards);
+    for (let i = 0; i < cards.length; i++) {
+        deck.innerHTML = '';
+        [].forEach.call(cards, function(addedCard) {
+            deck.appendChild(addedCard);
+        });
+        cards[i].classList.remove('show', 'open', 'match', 'disabled');
+    }
+    console.log('game has started')
 }
 
-function shuffleDeck() {
-    
+function shuffleDeck(cardArray) {
+    let currentIndex = cardArray.length, tempVal, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        tempVal = cardArray[currentIndex];
+        cardArray[currentIndex] = cardArray[randomIndex];
+        cardArray[randomIndex] = tempVal;
+    }
+    return cardArray;
     console.log('deck is shuffled');
 }
 
-function marioAudio() {
-    let marioAudio = new Audio('https://vgmdownloads.com/soundtracks/super-mario-bros/khbnvkqp/01%20-%20Super%20Mario%20Bros.mp3');
-    marioAudio.loop = false;
-    marioAudio.play();
+function startingAudio() {
+    let gameStartAudio = new Audio('https://www.superluigibros.com/downloads/sounds/NES/SMB3/mp3/smb3-overworld.mp3');
+    gameStartAudio.loop = false;
+    gameStartAudio.play();
 }
 
+function winnerAudio() {
+    let winnerAudio = new Audio('https://themushroomkingdom.net/sounds/wav/smb/smb_stage_clear.wav');
+    winnerAudio.loop = false;
+    winnerAudio.play();
+}
+
+function loserAudio() {
+    let loserAudio = new Audio('https://themushroomkingdom.net/sounds/wav/smb/smb_mariodie.wav');
+    loserAudio.loop = false;
+    loserAudio.play();
+}
+
+function displayCard() {
+    this.classList.toggle('openedCard');
+    this.classList.toggle('showCard');
+    this.classList.toggle('disabledCard');
+}
